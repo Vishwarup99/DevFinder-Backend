2e8b14e8854d2ad626887f4a0bf6908b69984b76
Vishwarup99 <svishwarup@gmail.com>
2025-06-25 06:33:28 +0530

    Added .env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.DB_CONNECTION);
 }
 
 module.exports = connectDB;
